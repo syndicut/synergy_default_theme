2ae3ddb95d41f8c15b43091f812d9fcd5cab3276
Rashit Azizbaev <syndicut@yandex-team.ru>
2012-04-26 15:44:46 +0400

    Fixed override to replace

--- app/overrides/sdt_products_list_item.rb
+++ app/overrides/sdt_products_list_item.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(
     :virtual_path => "shared/_products",
-    :insert_bottom => "[data-hook='products_list_item']",
+    :replace => "[data-hook='products_list_item']",
     :partial => "shared/product",
     :name => "sdt_products_list_item")
