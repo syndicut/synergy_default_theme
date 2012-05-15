551a73722f181117987eccf2d53b3d17e3f09200
Rashit Azizbaev <syndicut@gmail.com>
2012-05-15 16:30:43 +0400

    Final fix for jquery.rating.js

--- app/overrides/sdt_sr_footer_right.rb
+++ app/overrides/sdt_sr_footer_right.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
     :virtual_path => "layouts/spree_application",
     :insert_bottom => "[data-hook='footer_right']",
-    :text => '<%= javascript_include_tag("javascripts/store/jquery.rating.js") %>
+    :text => '<%= javascript_include_tag("store/jquery.rating") %>
               <%= javascript_tag("$(document).ready(function(){$(\'.stars\').rating({required:true});});") %>',
     :name => "sdt_sr_footer_right") if defined?(SpreeReviews)
