76c77e500fdd30df61ccac03cb7c9377c86837b1
Rashit Azizbaev <syndicut@gmail.com>
2012-05-15 18:17:03 +0400

    Reverted selector

--- app/overrides/sdt_sr_footer_right.rb
+++ app/overrides/sdt_sr_footer_right.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
     :virtual_path => "layouts/spree_application",
     :insert_bottom => "[data-hook='footer_right']",
     :text => '<%= javascript_include_tag("store/jquery.rating") %>
-              <%= javascript_tag("$(document).ready(function(){$(\'.star\').rating({required:true});});") %>',
+              <%= javascript_tag("$(document).ready(function(){$(\'.stars\').rating({required:true});});") %>',
     :name => "sdt_sr_footer_right") if defined?(SpreeReviews)
