b43aabdac7df1d96e2c106013c5668ccdada9bb0
Rashit Azizbaev <syndicut@gmail.com>
2012-05-15 16:39:48 +0400

    Fixed path to review.css

--- app/overrides/sdt_sr_inside_head.rb
+++ app/overrides/sdt_sr_inside_head.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(
     :virtual_path => "layouts/spree_application",
     :insert_bottom => "[data-hook='inside_head']",
-    :text => "<%= stylesheet_link_tag('reviews.css') %>",
+    :text => "<%= stylesheet_link_tag('store/reviews') %>",
     :name => "sdt_sr_inside_head") if defined?(SpreeReviews)
